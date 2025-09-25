2cddcf5a61e6f90e951cf43c244a797392d6a550
kaede-rukawa <e0816231@u.nus.edu>
2025-09-25 23:24:04 +0800

    test

--- algorithm_codes/binary_search.py
+++ algorithm_codes/binary_search.py
@@ -2,22 +2,16 @@
 # @Time    : 2025/9/17 21:37
 # @Author  : kaede
 # @File    : binary_search.py
-def binary_serch(arr, target):
-    start, end = 0, len(arr) - 1
-    while True:
-        if end <= start:
-            if target == arr[start]:
-                return start
-            elif target == arr[end]:
-                return end
-            else:
-                return None
+def binary_search(arr, target):
+    start, end = 0, len(arr)-1
+    while start <= end:
         mid = (start + end) // 2
-        if arr[mid] > target:
-            end = mid
-        elif arr[mid] < target:
-            start = mid
-        else:
+        if arr[mid] == target:
             return mid
+        elif arr[mid] > target:
+            end = mid - 1
+        elif arr[mid] < target:
+            start = mid + 1
+    return None
 arr = [1,2,3,4,5,6,7,9]
-print(binary_serch(arr,5))
+print(binary_search(arr,5))
